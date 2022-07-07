70e9ac18be0f3b6ef499ec91bb2ee62fa324f7ea
lanczyatt <lanczyatt@gmail.com>
2022-07-07 11:09:25 -0700

    add függvény módosítás

--- js/funkciok.js
+++ js/funkciok.js
@@ -1,3 +1,12 @@
 function add(x, b, c){
     return x + b + c;
     }
+
+function add2(...a){
+    let sum = 0;
+    for(let i = 0; i < a.length; i++){
+        sum += a[i];
+    }
+
+    return sum;
+}
